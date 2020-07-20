47ddad885ae15ce96ae243faabe1c27e6a96cb80
szh <szh@dareway.com.cn>
2020-07-20 17:30:19 +0800

    完成登录页面样式

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './api'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/icon.css';
 
 Vue.prototype.$api = api;
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, {
+    size: 'small'
+});
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,14 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: '/',
+      path: '/helloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     },
     {
-      path: '/index2',
-      name: '测试跳转',
-      component: (resolve) => require(['@/page/index'], resolve),
+      path: '/',
+      name: '用户登录',
+      component: (resolve) => require(['@/page/login'], resolve),
     }
   ]
 })
